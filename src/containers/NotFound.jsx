a9f24e98ad352ead51ecd12b9397f5c7b19b8467
Roberto Cervantes <robert.hct@gmail.com>
2020-04-10 09:40:14 -0500

    Se agregó la página (container) NotFound, h1 dentro de fragments, con ruta por defecto

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>Página no encontrada</h1>
+    <h2>Regresa a la página de <a href="/">home</a>
+    </h2>
+  </>
+);
+
+export default NotFound;
